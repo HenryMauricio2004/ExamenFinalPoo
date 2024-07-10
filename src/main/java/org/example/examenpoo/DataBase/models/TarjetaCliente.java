4b54a4ba1415c05d48874708273459fe99e80117
M4t7e021 <00030123@uca.edu.sv>
2024-07-09 21:22:08 -0600

    00030123 Subiendo nuevos cambios

--- src/main/java/org/example/examenpoo/DataBase/models/TarjetaCliente.java
+++ src/main/java/org/example/examenpoo/DataBase/models/TarjetaCliente.java
@@ -6,12 +6,14 @@ public class TarjetaCliente { //00082023 Definición de la clase TarjetaCliente
     private String tipoTarjeta; //00082023 Tipo de tarjeta (crédito/débito)
     private String numTarjeta; //00082023 Número de la tarjeta
     private Date fechaExpiracion; //00082023 Fecha de expiración de la tarjeta
+    private String facilitador;// 00030123 El tipo de facilitador que ocupa la tarjeta
 
-    public TarjetaCliente(int idTarjeta, String tipoTarjeta, String numTarjeta, Date fechaExpiracion) { //00082023 Constructor de la clase
+    public TarjetaCliente(int idTarjeta, String tipoTarjeta, String numTarjeta, Date fechaExpiracion, String facilitador) { //00082023 Constructor de la clase
         this.idTarjeta = idTarjeta; //00082023 Asigna el identificador de la tarjeta
         this.tipoTarjeta = tipoTarjeta; //00082023 Asigna el tipo de tarjetaq
         this.numTarjeta = numTarjeta; //00082023 Asigna el número de la tarjeta
         this.fechaExpiracion = fechaExpiracion; //00082023 Asigna la fecha de expiración
+        this.facilitador = facilitador;
     } //00082023 Fin del constructor
 
     public int getIdTarjeta() { return idTarjeta; } //00082023 Obtiene el identificador de la tarjeta
@@ -29,4 +31,12 @@ public class TarjetaCliente { //00082023 Definición de la clase TarjetaCliente
     public Date getFechaExpiracion() { return fechaExpiracion; } //00082023 Obtiene la fecha de expiración
 
     public void setFechaExpiracion(Date fechaExpiracion) { this.fechaExpiracion = fechaExpiracion; } //00082023 Establece la fecha de expiración
+
+    public String getFacilitador() {
+        return facilitador;//00030123 obtiene el facilitador de la tarjeta
+    }
+
+    public void setFacilitador(String facilitador) {
+        this.facilitador = facilitador;//0003123 Establece el tipo de facilitador que es la TARJETA
+    }
 } //00082023 Fin de la clase TarjetaCliente
