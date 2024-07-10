c9ba06e5201a9118cc4b4c0e1d8ef86c3ac991f8
M4t7e021 <00030123@uca.edu.sv>
2024-07-09 21:21:59 -0600

    00030123 Subiendo nuevos cambios

--- src/main/java/com/mateouca/examen_finalpoo/BCNController.java
+++ src/main/java/com/mateouca/examen_finalpoo/BCNController.java
@@ -9,6 +9,8 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.net.URL;
+import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class BCNController implements Initializable {
@@ -26,15 +28,15 @@ public class BCNController implements Initializable {
     @FXML
     private TableColumn colMonto;//0030123 La columna donde se mostrara el monto gastado del cliente del BCN
     @FXML
-    private Button btnCompras , btnDinero , btnTarjetas , btnFacilitador;//00030123 seran los botenes de las opciones que se pueden realizar a la hora de consutar la base de datos
-    @FXML
     private TextArea txtDescrip;//00030123 En este TextArea se mostrara una pequeña informacion de lo que hace cada opcion de la vista del cliente del BCN
 
     private ObservableList<ClientBCN> clientBCN;//00030123 es una lista observable de los clientes del BCN
+    //private DataBaseController dataBaseController //00030123 es el controlador de la base de datos
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         clientBCN = FXCollections.observableArrayList();//0030123 es crecar un ObservableList vacio para almacenar e ir guardando a los clientes del BCN
+        TableView<ClientBCN> tblclient = new TableView<>();//00030123 crea la nueva tabla de los clientes del BCN
         this.tblclient.setItems(clientBCN);//00030123 es para refrescar la tabla de los clientes del BCN y que se vayan mostrado a la tabla
 
         this.colNombres.setCellFactory(new PropertyValueFactory("name"));//00030123 para asociar las columnas al tema del modelo de la tabla de los clientes del BCN y que sepa que objeto tomar cada columna
@@ -42,11 +44,33 @@ public class BCNController implements Initializable {
         this.colIDCliente.setCellFactory(new PropertyValueFactory("id"));//00030123 para asociar las columnas al tema del modelo de la tabla de los clientes del BCN y que sepa que objeto tomar cada columna
         this.colTarjeta.setCellFactory(new PropertyValueFactory("card"));//00030123 para asociar las columnas al tema del modelo de la tabla de los clientes del BCN y que sepa que objeto tomar cada columna
         this.colMonto.setCellFactory(new PropertyValueFactory("money"));//00030123 para asociar las columnas al tema del modelo de la tabla de los clientes del BCN y que sepa que objeto tomar cada columna
+
+        try {
+            tblclient.getItems().addAll(ClientBCN.getComprasPorCliente());//00030123 es para refrescar la tabla de los clientes del BCN y que se vayan mostrado a la tabla
+        }catch (SQLException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Error al conectar con la base de datos");
+            alert.showAndWait();
+        }
     }
 
     @FXML
     public void onComprasRealizadas(ActionEvent event){
+        int id_tarjeta = Integer.parseInt(txtDescrip.getText());
+        float montoTotal = (float) colMonto.getCellData(colMonto.getTableView().getSelectionModel().getSelectedIndex());
+        LocalDate fecha = (LocalDate) colFechas.getCellData(colFechas.getTableView().getSelectionModel().getSelectedIndex());
 
+        try {
+            upDateCompra(id_tarjeta, montoTotal, fecha);
+        }catch (SQLException e){
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Error al conectar con la base de datos");
+            alert.showAndWait();
+        }
     }
 
     @FXML
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mateouca.examen_finalpoo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.mateouca.examen_finalpoo to javafx.fxml;
